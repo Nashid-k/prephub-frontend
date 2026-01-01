82b49b17f538f8dab07c2a6e957d239ebd2ca8c4
Nashid-k <nashidk1999@gmail.com>
2026-01-02 02:23:43 +0530

    fixes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "d41d8cd98f00b204e9800998ecf8427e"
   }, {
     "url": "index.html",
-    "revision": "0.61q39op41go"
+    "revision": "0.0hqud6p5thg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
